52293749c317ed3dbc5edb682d7dc38cee5bcf52
Davy <davyfungaloi@gmail.com>
2020-10-24 23:27:02 +0200

    added rating component & styling

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
@@ -14,12 +15,10 @@ const Product = ({ product }) => {
             <strong>{product.name}</strong>
           </Card.Title>
         </a>
-        <Card.Text as='div'>
-            <div className='my-3'>
-                {product.rating} from {product.numReviews} reviews
-            </div>
+        <Card.Text as="div">
+          <Rating value={product.rating} text={`${product.numReviews} reviews`}></Rating>
         </Card.Text>
-  <Card.Text as='h3'>${product.price}</Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
