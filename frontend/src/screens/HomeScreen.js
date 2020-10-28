6423e8645dd58891d6dda0dc2e755785b68b0d08
Davy <davyfungaloi@gmail.com>
2020-10-28 22:52:54 +0100

    added axios http request, also added proxy in package.json so we can communicate with backend more efficiently

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,21 +1,33 @@
-import React from 'react'
-import { Row, Col } from 'react-bootstrap'
-import products from '../products'
-import Product from '../components/Product'
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import axios from 'axios'
 
 const HomeScreen = () => {
-    return (
-        <>
-         <h1>Latest Products</h1>
-         <Row>
-             {products.map(product => (
-                 <Col key={product._id} sm={12} md={6} lg={4} xl={3} >
-                        <Product product={product}/>
-                 </Col>
-             ) )}
-             </Row>   
-        </>
-    )
-}
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      ///data is a descructured way of saying res, normmally its const res = aka res.data
+      setProducts(data);
+    };
+    fetchProducts()
+  }, []);
 
-export default HomeScreen
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
+
+///const [products, setProducts] = useState([])
