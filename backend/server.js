174970579766d00e8d8f62d1a266f3fc4b665a34
Davy <davyfungaloi@gmail.com>
2020-10-30 23:08:12 +0100

    added environment modules

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,27 @@
-const express = require('express')
-const products = require('./data/products')
+const express = require("express");
+const products = require("./data/products");
+const dotenv = require("dotenv");
 
-const app= express()
+dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send('API is running now')
-})
+const PORT = process.env.PORT || 5000;
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-   
-})
+const app = express();
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p=> p._id === req.params.id)
-    res.json(product)
-   
-})
+app.get("/", (req, res) => {
+  res.send("API is running now");
+});
 
-app.listen(5000,console.log('server running on port 5000'))
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
