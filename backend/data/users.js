c661813e76bcce684105d6fdd4057805475b7104
Davy <davyfungaloi@gmail.com>
2020-11-01 23:20:34 +0100

    added bcryptjs for encrypting pw

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+const users = [
+    {
+        name: 'Admin USer',
+        email: 'admin@example.com',
+        password: xxxx,
+        isAdmin: true,
+    },
+    {
+        name: 'Obiwan Kenobi',
+        email: 'highground@example.com',
+        password: xxxx,
+     
+    }
+        ,
+    {
+        name: 'Darth Maul',
+        email: 'robolegs@example.com',
+        password: xxxx,
+        isAdmin: true,
+    }
+
+
+]
