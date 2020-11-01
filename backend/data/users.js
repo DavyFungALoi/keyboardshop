c3271720f804e954f9ca4df3fe8756455d35686c
Davy <davyfungaloi@gmail.com>
2020-11-01 23:23:09 +0100

    added some dummy user data

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,23 +1,23 @@
+import bcrypt from "bcryptjs";
+
 const users = [
-    {
-        name: 'Admin USer',
-        email: 'admin@example.com',
-        password: xxxx,
-        isAdmin: true,
-    },
-    {
-        name: 'Obiwan Kenobi',
-        email: 'highground@example.com',
-        password: xxxx,
-     
-    }
-        ,
-    {
-        name: 'Darth Maul',
-        email: 'robolegs@example.com',
-        password: xxxx,
-        isAdmin: true,
-    }
+  {
+    name: "Admin USer",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Obiwan Kenobi",
+    email: "highground@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Darth Maul",
+    email: "robolegs@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+];
 
-
-]
+export default users
