e899893320ffc3793547841c4bf2143e9252a454
Davy <davyfungaloi@gmail.com>
2020-11-07 22:30:31 +0100

    added a seperate middleware file, added errorhandling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
-import express from "express"
-import dotenv from "dotenv"
-import connectDB from './config/db.js'
-import colors from 'colors'
-import productRoutes from './routes/productRoutes.js'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import productRoutes from "./routes/productRoutes.js";
+
 
 dotenv.config();
-connectDB()
+connectDB();
 
 const app = express();
 
@@ -13,13 +15,28 @@ app.get("/", (req, res) => {
   res.send("API is running now");
 });
 
+app.use("/api/products", productRoutes);
 
-app.use('/api/products', productRoutes)
+app.use(notFound)
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 5000;
 
-
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+
+/*
+
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`)
+  res.status(404)
+  next(error)
+})
+*/
