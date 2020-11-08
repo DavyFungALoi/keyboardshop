5c3f73afb45513c8734cf36dfad24873f2c9e1a5
Davy <davyfungaloi@gmail.com>
2020-11-08 23:12:15 +0100

    changed req.statusCode to res.statuscode to fix error handling

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -6,12 +6,14 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode == 200 ? 500 : req.statusCode;
-  res.status(statusCode);
+  const statusCode = res.statusCode == 200 ? 500 : res.statusCode
+  res.status(statusCode)
   res.json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
+
+
 
 export { notFound, errorHandler };
