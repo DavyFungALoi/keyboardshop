7df71c9d9f5c4189dbc2a486d00edcccdc6ae38b
Davy <davyfungaloi@gmail.com>
2020-11-20 23:12:04 +0100

    added Add to Cart Functionality for state using a spread operator on the productCountInStock to determine amount

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
         <Container>
           <Route path='/' component={HomeScreen} exact/>
           <Route path='/product/:id' component={ProductScreen}/>
+          <Route path='/cart/:id?' component={CartScreen}/>
         </Container>
       </main>
       <Footer />
