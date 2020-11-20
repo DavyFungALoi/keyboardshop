92ed005040abe2f1d524f0cbd0d8fd841b703a88
Davy <davyfungaloi@gmail.com>
2020-11-21 00:13:11 +0100

    added dispatch and selector with useEffect to CartScreen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -6,7 +6,7 @@ export const addToCart =(id, qty) => async (dispatch, getState) => {
     dispatch ({
         type: CART_ADD_ITEM,
         payload: {
-            product: data_.id,
+            product: data._id,
             name: data.name,
             image: data.image,
             price: data.price,
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -16,7 +16,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { listProductDetails } from "../actions/productActions";
 
 const ProductScreen = ({history,  match }) => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
