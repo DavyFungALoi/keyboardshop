30b7479cad8aaef408a130d2f6bb2abcc79272fd
Davy <davyfungaloi@gmail.com>
2020-11-30 15:57:29 +0100

    added baseline skeleton for Loginscreen, added a component Formctainer for reuse

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -18,10 +18,12 @@ export const login = (email, password => async (dispatch) => {
         })
         localStorage.setItem('userInfo', JSON.stringify(data))
     } catch (error) {   
-        type: USER_LOGIN_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload:
+              error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
+          });
     }
 })
---
+++ frontend/src/components/FormContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Container, from, Col, Row} from 'react-bootstrap'
+
+export const FormContainer = ({children}) => {
+    return (
+        <Container>
+            <Row className='justify-content-md-center'>
+                <Col xs={12} md={6}>
+                    {children}
+                </Col>
+            </Row>
+        </Container>
+    )
+}
