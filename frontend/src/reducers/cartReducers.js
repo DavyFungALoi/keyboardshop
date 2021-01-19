8d889cf9aa2be9487c6ef59faaf16e276ae23682
Davy <davyfungaloi@gmail.com>
2021-01-19 08:52:25 +0100

    added shipping information & state, actions & reducers

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,7 +1,13 @@
-import { CART_ADD_ITEM, CARD_REMOVE_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from "../constants/cartConstants";
 
-
-export const cartReducer = (state = { cartItems: [] }, action) => {
+export const cartReducer = (
+  state = { cartItems: [], shippingAddress: {} },
+  action
+) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
@@ -16,10 +22,16 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
       } else {
         return { ...state, cartItems: [...state.cartItems, item] };
       }
-      case CART_REMOVE_ITEM:
-        return{
-          ...state, cartItems: state.cartItems.filter(x=> x.product !==action.payload)
-        }
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      };
+    case CART_SAVE_SHIPPING_ADDRESS:
+      return {
+        ...state,
+        shippingAddress: action.payload,
+      };
 
     default:
       return state;
@@ -27,3 +39,5 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
 };
 
 ///ExistItem = checking whether this item even exists
+
+/// Remove_ITEM = : copy the state with spread operator, then set the CartItems to the current state and filter out for all of the products that do NOT equal the current selected product.
