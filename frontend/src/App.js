c100b1a6f24ab4c14197b03eb1739489eb15bbf9
Davy <davyfungaloi@gmail.com>
2021-01-21 08:35:59 +0100

    added payment method screen en action & reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import { LoginScreen } from "./screens/LoginScreen";
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
 
 
 const App = () => {
@@ -19,10 +20,11 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Route path='/' component={HomeScreen} exact/>
-          <Route path='/shipping' component={ShippingScreen} exact/>
+          <Route path='/shipping' component={ShippingScreen}/>
+          <Route path='/payment' component={PaymentScreen}/>
           <Route path='/login/' component={LoginScreen}/>
           <Route path='/register/' component={RegisterScreen}/>
-          <Route path='/profile/' component={ProfileScreen}/>
+          <Route path='/profile/' component={ProfileScreen}/>s
           <Route path='/product/:id' component={ProductScreen}/>
           <Route path='/cart/:id?' component={CartScreen}/>
         </Container>
