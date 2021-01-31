4d5eff5cdfa80c16429fe885dcfdbaed3a8ee07f
Davy <davyfungaloi@gmail.com>
2021-01-31 14:54:06 +0100

    added dropdown sections for admin profile areas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/user/admin" component={UserListScreen} />
+          <Route path="/admin/userlist" component={UserListScreen} />
           <Route path="/order/:id" component={OrderScreen} />
           <Route path="/" component={HomeScreen} exact />
           <Route path="/placeorder" component={PlaceOrderScreen} />
@@ -29,7 +29,6 @@ const App = () => {
           <Route path="/payment" component={PaymentScreen} />
           <Route path="/login/" component={LoginScreen} />
           <Route path="/register/" component={RegisterScreen} />
-          <Route path="/profile/" component={ProfileScreen} />
           <Route path="/profile/" component={ProfileScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -11,6 +11,9 @@ const UserListSCreen = () => {
   const dispatch = useDispatch();
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
+  const deleteHandler = () => {
+    console.log('delete')
+  }
 
   useEffect(() => {
     dispatch(listUsers());
@@ -39,6 +42,27 @@ const UserListSCreen = () => {
                 <td>{user.name}</td>
                 <td>
                   <a href={`mailto:${user.email}`}>{user.email}</a>
+                </td>
+                <td>
+                  {user.isAdmin ? (
+                    <i className="fas fa-check" style={{ color: "green" }}></i>
+                  ) : (
+                    <i className="fas fa-times" style={{ color: "red" }}></i>
+                  )}
+                </td>
+                <td>
+                  <LinkContainer to={`/user/${user._id}/edit`}>
+                    <Button variant="light" className="btn-sm">
+                      <i className="fas fa-edit"></i>
+                    </Button>
+                  </LinkContainer>
+                  <Button
+                    variant="danger"
+                    className="btn-sm"
+                    onClick={() => deleteHandler(user._id)}
+                  >
+                    <i className="fas fa-trash"></i>
+                  </Button>
                 </td>
               </tr>
             ))}
