e861a242f879be58d0ab404d624fa19bd4a34ad9
Davy <davyfungaloi@gmail.com>
2021-02-09 19:43:01 +0100

    added morgan to have an overview of routes logged in the console

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from 'path'
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import morgan from 'morgan'
 import colors from "colors";
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import productRoutes from "./routes/productRoutes.js";
@@ -10,10 +11,14 @@ import orderRoutes from "./routes/orderRoutes.js"
 import uploadRoutes from "./routes/uploadRoutes.js"
 
 
+
 dotenv.config();
 connectDB();
 
 const app = express();
+if(process.env.NODE_ENV==='development') {
+app.use(morgan('dev'))
+}
 
 app.use(express.json()) ///enables json parsing in the body
 
