c87568859f68961f7b1cfdb4993d92ad6cf718a4
Davy <davyfungaloi@gmail.com>
2021-02-14 13:42:34 +0100

    aded action reducer and constant

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,7 +7,8 @@ import {
   deleteProduct,
   createProduct,
   updateProduct,
-  createProductReview
+  createProductReview,
+  getTopProducts
 } from "../controllers/productController.js";
 
 router.route("/").get(getProducts).post(protect, admin,createProduct)
@@ -15,5 +16,7 @@ router.route("/").get(getProducts).post(protect, admin,createProduct)
 router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct);
 
 router.route("/:id/reviews").post(protect,createProductReview)
+
+router.get('/top', getTopProducts)
 
 export default router;
